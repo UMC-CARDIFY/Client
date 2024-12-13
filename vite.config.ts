95db677825223b93182b75a0f5a895dee045809b
Sohyunnnn <thgus05061@naver.com>
2024-12-14 00:10:30 +0900

    build: add svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import biomePlugin from "vite-plugin-biome";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    svgr({
+      include: "**/*.svg?react",
+    }),
     biomePlugin({
       mode: "check",
       files: ".",
