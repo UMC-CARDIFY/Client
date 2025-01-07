aa352068cf05f294ab7f9637b2c2fc7f42656d20
Dahyun <124647898+daahyunk@users.noreply.github.com>
2025-01-08 00:00:35 +0900

    [Init/#3] 프로젝트 절대 경로 설정 및 관련 설정 추가 (#4)
    * feat: 프로젝트 절대 경로 설정 및 관련 설정 추가
    
    * feat : 폴더 구조 수정, 아카이브 페이지 경로 설정
    
    * feat : 아카이브 페이지 경로 설정, router 수정
    
    * feat : 폴더 구조 세팅
    
    * feat : 폴더 구조 세팅 추가...
    
    * feat : asset 폴더 안 svgs, images 폴더 생성
    
    ---------
    
    Co-authored-by: dhdld <btj5420@naver.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import { Archive } from "../pages";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      {
-        //
-      },
-    ],
+  },
+  {
+    path: "/archive",
+    element: <Archive />,
   },
 ]);
 
