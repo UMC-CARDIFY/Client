018fb9dafac6de87c3ad98709919815438678f81
dhdld <btj5420@naver.com>
2025-01-07 15:10:56 +0900

    feat : 아카이브 페이지 경로 설정, router 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
---
+++ src/pages/archive/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Archive = () => {
+  return (
+    <div>
+      <h2>Archive 페이지</h2>
+    </div>
+  );
+};
