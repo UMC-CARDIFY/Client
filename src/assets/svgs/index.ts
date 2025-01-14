599eeb0399b771394f36af57ecd0b94589c69d44
hyesngy <tell280210@gmail.com>
2025-01-14 12:22:52 +0900

    feat: add archive svgs

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,8 @@
+export { default as SearchIcon } from "./ic_navigation_search_20.svg?react";
+export { default as MypageIcon } from "./ic_navigation_mypage_28.svg?react";
+export { default as KebabIcon } from "./ic_archive_kebab_24.svg?react";
+export { default as HalfArrowLightIcon } from "./ic_common_halfarrowlight_16.svg?react";
+export { default as InnerFolderIcon } from "./ic_archive_innerfolder_28.svg?react";
+export { default as NewFolderIcon } from "./ic_archive_newfolder_24.svg?react";
+export { default as PlusIcon } from "./ic_archive_plus_12.svg?react";
+export { default as Logo } from "./logo.svg?react";
