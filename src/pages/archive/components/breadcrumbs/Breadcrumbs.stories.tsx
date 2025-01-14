72bc7feeb3bbee616d1ac86ee205dd7db19ff175
hyesngy <tell280210@gmail.com>
2025-01-14 12:36:39 +0900

    feat: 브레드크럼 컴포넌트 추가

---
+++ src/pages/archive/components/breadcrumbs/Breadcrumbs.stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, StoryFn } from "@storybook/react";
+import React from "react";
+import Breadcrumbs from "./Breadcrumbs";
+
+export default {
+  title: "Archive/Components/Breadcrumbs",
+  component: Breadcrumbs,
+  argTypes: {
+    username: {
+      control: { type: "text" },
+      description: "사용자 이름",
+    },
+    pathSegments: {
+      control: { type: "object" },
+      description: "현재 경로를 나타내는 문자열 배열",
+    },
+  },
+} as Meta<typeof Breadcrumbs>;
+
+const Template: StoryFn<typeof Breadcrumbs> = (args) => <Breadcrumbs {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  username: "혜성",
+  pathSegments: ["프로젝트"],
+};
+
+export const PathSegments = Template.bind({});
+PathSegments.args = {
+  username: "혜성",
+  pathSegments: ["카디파이", "프론트엔드"],
+};
+
+export const EmptyPathSegments = Template.bind({});
+EmptyPathSegments.args = {
+  username: "혜성",
+  pathSegments: [],
+};
