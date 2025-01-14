4d0e8716c426a3442e1779379a74bf2b5a539b28
hyesngy <tell280210@gmail.com>
2025-01-14 20:45:59 +0900

    chore: NewFolder 컴포넌트 수정 및 webp 추가

--- src/pages/archive/components/newFolder/NewFolder.tsx
+++ src/pages/archive/components/newFolder/NewFolder.tsx
@@ -1,12 +1,9 @@
-import { NewFolderIcon, PlusIcon } from "@svgs/index";
+import { NewfolderImage } from "@images/index";
 import React from "react";
 
 const NewFolder = () => {
   return (
-    <div className="w-[188px] py-3 rounded-lg border border-dashed border-gray-300 flex flex-row justify-center items-center hover:bg-gray-50 cursor-pointer">
-      <NewFolderIcon />
-      <PlusIcon />
-    </div>
+    <img src={NewfolderImage} alt="new folder" className="w-[11.75rem] h-[3rem] hover:bg-gray-50 cursor-pointer" />
   );
 };
 
