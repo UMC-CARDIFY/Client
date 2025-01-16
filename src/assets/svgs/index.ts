f7e65442d4a7d66284be8f58d4ca82e7034b1d26
Sohyunnnn <thgus05061@naver.com>
2025-01-16 20:03:30 +0900

    chore: add assets

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,4 @@
+export { default as CheckboxIcon } from "./ic_checkbox.svg?react";
+export { default as ArchiveNoteIcon } from "./ic_archive_note.svg?react";
+export { default as StarIcon } from "./ic_archive_star.svg?react";
+export { default as FlashcardIcon } from "./ic_common_flashcard.svg?react";
