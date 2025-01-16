bdef53687ea6e90c227eaad6708b551812483503
dhdld <btj5420@naver.com>
2025-01-16 14:50:44 +0900

    feat : alert 경고창 svg 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,3 +1,4 @@
 export { default as IcArchiveFolder } from "./ic_archive_folder.svg?react";
 export { default as IcColorCircle } from "./ic_color_circle.svg?react";
 export { default as IcColorCircleCheck } from "./ic_color_circle_check.svg?react";
+export { default as IcCommonAlert } from "./ic_common_alert.svg?react";
