75895da133b015771b33256d296facc5b3a31fb8
dhdld <btj5420@naver.com>
2025-01-17 15:59:06 +0900

    chore : 폴더 이동 모달에 필요한 svgs 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -2,3 +2,6 @@ export { default as ArchiveFolderIcon } from "./ic_archive_folder.svg?react";
 export { default as ColorCircleIcon } from "./ic_color_circle.svg?react";
 export { default as ColorCircleCheckIcon } from "./ic_color_circle_check.svg?react";
 export { default as CommonAlertIcon } from "./ic_common_alert.svg?react";
+export { default as CommonXIcon } from "./ic_common_x.svg?react";
+export { default as ArchiveFilledStarIcon } from "./ic_archive_filledstar.svg?react";
+export { default as SearchThinIcon } from "./ic_search-thin.svg?react";
