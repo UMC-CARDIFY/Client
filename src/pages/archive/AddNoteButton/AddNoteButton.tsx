a5307bec2af67e5e7ddb3bf5f31d8e393897e74c
hyesngy <tell280210@gmail.com>
2025-01-21 16:28:46 +0900

    fix: 노트추가버튼 아이콘 color 추가

--- src/pages/archive/AddNoteButton/AddNoteButton.tsx
+++ src/pages/archive/AddNoteButton/AddNoteButton.tsx
@@ -2,8 +2,8 @@ import { ArchiveNoteIcon, PlusIcon } from "@svgs/index";
 
 const AddNoteButton: React.FC = () => {
   return (
-    <div className="items-center inline-flex py-[0.5rem] pr-[0.5rem] pl-[0.62rem] gap-[0.12rem] bg-gray-100 rounded-lg">
-      <ArchiveNoteIcon />
+    <div className="items-center inline-flex py-[0.5rem] pr-[0.5rem] pl-[0.62rem] gap-[0.12rem] bg-gray-100 rounded-lg cursor-pointer">
+      <ArchiveNoteIcon className="fill-gray-450" />
       <PlusIcon className="w-4 h-4" />
     </div>
   );
