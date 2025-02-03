4d592dd5f4d66e28a9235263906096e1e38170df
hyesngy <tell280210@gmail.com>
2025-02-03 23:21:44 +0900

    feat: 예정된 학습 List 컴포넌트 구현

---
+++ src/pages/home/components/flashcard/ScheduledLearningList.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import ScheduledLearningItem from "./ScheduledLearningItem";
+
+interface ScheduledLearningItemProps {
+  noteid: number;
+  name: string;
+  folderId: number;
+  folderName: string;
+  folderColor: string;
+  completedCards: number; // 학습 완료한 카드 수
+  timeReachedCards: number; // 학습 시간에 도달한 카드 수
+}
+
+interface ScheduledLearningListProps {
+  items?: ScheduledLearningItemProps[];
+}
+
+const ScheduledLearningList: React.FC<ScheduledLearningListProps> = ({ items = [] }) => {
+  if (!items?.length) {
+    return (
+      <div className="flex flex-col w-[52rem] h-60 items-center justify-center text-gray-400">
+        예정된 학습이 없습니다.
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-full flex flex-row gap-4">
+      {items.map((item) => (
+        <ScheduledLearningItem
+          key={item.noteid}
+          noteid={item.noteid}
+          name={item.name}
+          folderId={item.folderId}
+          folderName={item.folderName}
+          folderColor={item.folderColor}
+          completedCards={item.completedCards}
+          timeReachedCards={item.timeReachedCards}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ScheduledLearningList;
