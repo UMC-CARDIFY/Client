ab3429608c7ed139f3b3a4334886fc2dc72e9082
hyesngy <tell280210@gmail.com>
2025-02-04 01:16:54 +0900

    chore: App 파일 삭제

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import MainLayout from "../layouts/mainLayout";
 import { Archive, Flashcard, Home, Library, Mypage } from "../pages";
 import { PATHS } from "./paths";
