6d63382f8814e34e101b10e92dd5ebef4e0e430a
hyesngy <tell280210@gmail.com>
2025-02-03 23:13:59 +0900

    fix: Home 라우팅 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import MainLayout from "../layouts/mainLayout";
-import { Archive, Flashcard, Library, Mypage } from "../pages";
+import { Archive, Flashcard, Home, Library, Mypage } from "../pages";
 import { PATHS } from "./paths";
 
 const router = createBrowserRouter([
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: PATHS.HOME,
-        element: <App />,
+        element: <Home />,
       },
       {
         path: PATHS.ARCHIVE,
