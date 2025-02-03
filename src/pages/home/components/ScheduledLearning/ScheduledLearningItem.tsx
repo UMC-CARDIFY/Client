e1d683852881c9a67bda6ad6b10cc74def75ba5c
hyesngy <tell280210@gmail.com>
2025-02-04 01:24:32 +0900

    fix: 예정된 학습 디자인 수정

--- src/pages/home/components/ScheduledLearning/ScheduledLearningItem.tsx
+++ src/pages/home/components/ScheduledLearning/ScheduledLearningItem.tsx
@@ -23,20 +23,21 @@ const ScheduledLearningItem: React.FC<ScheduledLearningProps> = ({
 
   return (
     <div
-      className={`w-[16rem] h-[15rem] justify-center px-8 py-6 flex flex-col gap-2 bg-${folderColor}-100 rounded-lg cursor-pointer`}
+      className={`w-[16rem] h-[15rem] px-6 py-8 flex flex-col gap-2 bg-${folderColor}-100 rounded-lg cursor-pointer`}
     >
-      <HomeFlashcardIcon className={`fill-${folderColor}-600 stroke-${folderColor}-600 shrink-0`} />
-      <Text variant={"sub_heading3"} className="text-gray-500 mt-2 truncate">
-        {folderName}
-      </Text>
-      <Text variant={"sub_heading1"} className="text-base-black truncate">
-        {name}
-      </Text>
-      <Text variant={"heading3"} className="text-base-black">
-        {timeReachedCards}개
-      </Text>
-
-      <div className="relative w-52 mt-[22px]">
+      <div className="flex flex-col flex-1 gap-2">
+        <HomeFlashcardIcon className={`fill-${folderColor}-600 stroke-${folderColor}-600 shrink-0`} />
+        <Text variant={"sub_heading3"} className="text-gray-500 mt-2 truncate">
+          {folderName}
+        </Text>
+        <Text variant={"sub_heading1"} className="text-base-black truncate">
+          {name}
+        </Text>
+        <Text variant={"heading3"} className="text-base-black">
+          {timeReachedCards}개
+        </Text>
+      </div>
+      <div className="relative w-52">
         <div className={`w-full h-2 rounded-lg bg-${folderColor}-400`} />
         <div
           className={`absolute top-0 left-0 h-2 rounded-lg bg-${folderColor}-600 transition-all duration-300`}
