907292b2da5014d77babccff3c9f1d31a0254ecd
김소연 <54532519+dhdld@users.noreply.github.com>
2025-02-06 11:47:32 +0900

    [Feat/#12] 아카이브 메인 화면 폴더 있는 상태 UI 구현 (#14)
    * chore : 스토리북 모달 경로 수정
    
    * chore : 메인 화면에서 새 폴더 생성 svg 추가
    
    * feat : 메인 폴더리스트 구현 중...
    
    * feat : 메인 폴더 컴포넌트 ui 구현
    
    * feat : 폴더 추가 버튼 구현 및 메인 화면 ui 수정
    
    * chore : 불필요한 코드 제거
    
    * chore : 불필요한 svg 삭제
    
    * feat : 스타일 수정
    
    * chore : 새 폴더 추가 버튼 webp 파일로 변경
    
    * feat : 폴더 이름 작성시 엔터 불가
    
    * chore : 폴더 색상 타입 지정
    
    * refact: folderItem 데이터 구조 변경
    
    * chore : 버튼 간격 수정

---
+++ src/mocks/FolderItemData.ts
@@ -0,0 +1,85 @@
+interface FolderItemDataProps {
+  id: number;
+  folderName: string;
+  createdAt: string;
+  noteCount: number;
+  folderColor: string;
+  markState?: boolean;
+}
+
+const FolderItemData: FolderItemDataProps[] = [
+  {
+    id: 1,
+    folderName: "포근한 핫팩",
+    createdAt: "24/08/20",
+    noteCount: 10,
+    folderColor: "blue",
+    markState: false,
+  },
+  {
+    id: 2,
+    folderName: "휴먼미디어인터랙션디자인개론",
+    createdAt: "24/08/20",
+    noteCount: 100,
+    folderColor: "sage",
+    markState: true,
+  },
+  {
+    id: 3,
+    folderName: "웹 디자인 잘하는 방법",
+    createdAt: "24/08/20",
+    noteCount: 100,
+    folderColor: "orange",
+    markState: false,
+  },
+  {
+    id: 4,
+    folderName: "웹 디자인 잘하는 방법",
+    createdAt: "24/08/20",
+    noteCount: 100,
+    folderColor: "orange",
+    markState: false,
+  },
+  {
+    id: 5,
+    folderName: "웹 디자인 잘하는 방법",
+    createdAt: "24/08/20",
+    noteCount: 100,
+    folderColor: "orange",
+    markState: false,
+  },
+  {
+    id: 6,
+    folderName: "웹 디자인 잘하는 방법",
+    createdAt: "24/08/20",
+    noteCount: 100,
+    folderColor: "orange",
+    markState: false,
+  },
+  {
+    id: 7,
+    folderName: "웹 디자인 잘하는 방법",
+    createdAt: "24/08/20",
+    noteCount: 100,
+    folderColor: "orange",
+    markState: false,
+  },
+  {
+    id: 8,
+    folderName: "웹 디자인 잘하는 방법",
+    createdAt: "24/08/20",
+    noteCount: 100,
+    folderColor: "orange",
+    markState: false,
+  },
+  {
+    id: 9,
+    folderName: "웹 디자인 잘하는 방법",
+    createdAt: "24/08/20",
+    noteCount: 100,
+    folderColor: "orange",
+    markState: false,
+  },
+];
+
+export default FolderItemData;
