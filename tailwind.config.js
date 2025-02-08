7f7f3be9f76e4fc8ccd499314b35679b179df168
hyesngy <tell280210@gmail.com>
2025-02-08 21:06:09 +0900

    feat: 예정된 학습 hover 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,8 @@ export default {
       pattern: /fill-icon-(favorite|blue|ocean|lavender|mint|sage|gray|orange|coral|rose|plum)/,
     },
     {
-      pattern: /(bg|fill|stroke)-(blue|ocean|lavender|mint|sage|ashy|orange|coral|rose|plum)-(100|400|600)/,
+      pattern: /(bg|fill|stroke)-(blue|ocean|lavender|mint|sage|ashy|orange|coral|rose|plum)-(100|150|400|600)/,
+      variants: ["hover"],
     },
   ],
   theme: {
