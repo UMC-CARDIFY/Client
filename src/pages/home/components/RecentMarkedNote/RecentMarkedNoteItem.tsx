627558976f2e021359a314e0a83731378f77fd42
hyesngy <tell280210@gmail.com>
2025-02-08 20:52:42 +0900

    feat: 최근 즐겨찾기한 노트 hover 추가

--- src/pages/home/components/RecentMarkedNote/RecentMarkedNoteItem.tsx
+++ src/pages/home/components/RecentMarkedNote/RecentMarkedNoteItem.tsx
@@ -3,32 +3,20 @@ import { ArchiveNoteIcon, FlashcardIcon, StarIcon } from "@svgs/index";
 import React from "react";
 import { NoteItemProps } from "../../../../types/note";
 
-const RecentMarkedNoteItem: React.FC<NoteItemProps> = ({
-  name,
-  content,
-  folderColor,
-  editDate,
-  flashCardCount,
-}) => {
+const RecentMarkedNoteItem: React.FC<NoteItemProps> = ({ name, content, folderColor, editDate, flashCardCount }) => {
   const displayFlashcardNum = flashCardCount > 99 ? "99+" : flashCardCount;
   return (
-    <div className="w-[15.8125rem] h-[17.5rem] shrink-0 px-5 flex flex-col pb-4 border border-solid border-gray-150 rounded-lg cursor-pointer">
+    <div className="w-[15.8125rem] h-[17.5rem] shrink-0 px-5 flex flex-col pb-4 border border-solid border-gray-150 rounded-lg cursor-pointer hover:bg-brand-20">
       <div className="pr-1 pt-5 mb-3 flex flex-row items-center">
         <StarIcon className="cursor-pointer" />
         <ArchiveNoteIcon className={`mr-2 fill-${folderColor}-600`} />
-        <Text
-          variant={"sub_heading1"}
-          className="w-[9.8125rem] text-base-black truncate"
-        >
+        <Text variant={"sub_heading1"} className="w-[9.8125rem] text-base-black truncate">
           {name}
         </Text>
       </div>
       <hr />
       <div className="flex flex-col flex-1">
-        <Text
-          variant={"body3"}
-          className="mt-3 px-3 h-40 text-gray-500 line-clamp-6 whitespace-normal "
-        >
+        <Text variant={"body3"} className="mt-3 px-3 h-40 text-gray-500 line-clamp-6 whitespace-normal ">
           {content}
         </Text>
 
