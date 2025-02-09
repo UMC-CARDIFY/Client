64a14cebfb669af02d1f3df31643ee99c8a7349d
hyesngy <tell280210@gmail.com>
2025-02-10 01:04:39 +0900

    chore: 홈 화면 스토리 위치 수정

--- src/pages/home/index.stories.tsx
+++ src/pages/home/index.stories.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Home } from "./index";
 
 export default {
-  title: "Pages/Home",
+  title: "Home/Pages/HomePage",
   component: Home,
   parameters: {
     layout: "fullscreen",
