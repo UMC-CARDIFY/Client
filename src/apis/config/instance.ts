d95b3489927d620d21e87945a8b1ceaa9f9b59af
daahyunk <ekgusdl0408@naver.com>
2025-03-24 23:11:13 +0900

    feat: axios interceptor에 헤더 추가

--- src/apis/config/instance.ts
+++ src/apis/config/instance.ts
@@ -8,6 +8,14 @@ const axiosInstance: AxiosInstance = axios.create({
   withCredentials: true,
 });
 
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
