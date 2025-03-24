c96ad9a701773a74e1009d35f3f991aee1a981f0
daahyunk <ekgusdl0408@naver.com>
2025-03-25 03:24:56 +0900

    fix: export 방식 수정

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 export { Home } from "./home";
-export { Archive } from "./archive/pages";
+export { default as Archive } from "./archive/pages";
 export { Flashcard } from "./flashcard";
 export { Library } from "./library";
 export { Mypage } from "./mypage";
