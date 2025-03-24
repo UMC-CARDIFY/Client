5eceb6ab41df90bd4bf13123c5fc1650981b19d1
daahyunk <ekgusdl0408@naver.com>
2025-03-25 03:20:00 +0900

    refator: 실제 사용 중인 테마 컬러로 fallback하게 수정

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -5,5 +5,5 @@ export function isValidColor(color: string): color is keyof typeof colorMap {
 }
 
 export function getSafeColor(color: string): string {
-  return isValidColor(color) ? colorMap[color] : "#D9D9D9";
+  return isValidColor(color) ? colorMap[color] : colorMap["gray"];
 }
