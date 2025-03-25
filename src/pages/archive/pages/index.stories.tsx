1c91461bab5ae3631d205bff2ce995f5d3693a2f
daahyunk <ekgusdl0408@naver.com>
2025-03-25 17:27:00 +0900

    fix: ui test 에러 해결

--- src/pages/archive/pages/index.stories.tsx
+++ src/pages/archive/pages/index.stories.tsx
@@ -1,12 +1,19 @@
 import { Meta, StoryFn } from "@storybook/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Archive from "./index";
+
+const queryClient = new QueryClient();
 
 export default {
   title: "Archive/pages/Archive",
   component: Archive,
 } as Meta<typeof Archive>;
 
-const Template: StoryFn<typeof Archive> = () => <Archive />;
+const Template: StoryFn<typeof Archive> = () => (
+  <QueryClientProvider client={queryClient}>
+    <Archive />
+  </QueryClientProvider>
+);
 
 export const Default = Template.bind({});
 Default.args = {};
