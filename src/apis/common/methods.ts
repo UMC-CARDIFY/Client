1ddccfa7a51855f373c902b2e12b40c71bd807cc
Sohyunnnn <thgus05061@naver.com>
2025-03-31 03:27:28 +0900

    feat: patch 제네릭 함수 추가

--- src/apis/common/methods.ts
+++ src/apis/common/methods.ts
@@ -9,3 +9,8 @@ export const apiPost = async <T, B = undefined>(url: string, body?: B): Promise<
   const response = await axiosInstance.post<T>(url, body);
   return response.data;
 };
+
+export const apiPatch = async <T, B = undefined>(url: string, body?: B): Promise<T> => {
+  const response = await axiosInstance.patch<T>(url, body);
+  return response.data;
+};
