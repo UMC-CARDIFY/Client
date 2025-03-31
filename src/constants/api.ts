c5b4399a71702d353c2f65d98c80c3e3eaaaa773
hyesngy <tell280210@gmail.com>
2025-03-31 11:36:22 +0900

    fix: 폴더 삭제 API 경로 수정

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,6 @@
 export const END_POINTS = {
   GET_ACCESS_TOKEN: "/api/v1/auth/token",
   GET_SORT_FILTER: "/api/v1/folders/sort-filter",
-  DELETE_FOLDER: (folderId: number) => `api/v1/folders/${folderId}`,
+  DELETE_FOLDER: (folderId: number) => `/api/v1/folders/${folderId}`,
   MARK_FOLDER: (folderId: number) => `/api/v1/folders/${folderId}/mark-folders`,
 };
