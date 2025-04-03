58f58ef0ffb1ea79f1916a664281453ec76409a6
Sohyunnnn <thgus05061@naver.com>
2025-04-03 22:20:22 +0900

    chore: 엔드 포인트 수정

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,6 @@
 export const END_POINTS = {
-  GET_ACCESS_TOKEN: "/api/v1/auth/token",
-  GET_SORT_FILTER: "/api/v1/folders/sort-filter",
-  POST_FOLDERS: "/api/v1/folders/addFolder",
-  PATCH_FOLDERS: (folderId: number) => `/api/v1/folders/${folderId}`,
+  GET_ACCESS_TOKEN: "/auth/token",
+  GET_SORT_FILTER: "/folders/sort-filter",
+  POST_FOLDERS: "/folders/addFolder",
+  PATCH_FOLDERS: (folderId: number) => `/folders/${folderId}`,
 };
