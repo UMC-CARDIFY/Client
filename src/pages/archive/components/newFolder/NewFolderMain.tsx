d333151f0c9d643896f6bd7242ef233fbefc9aa1
Sohyunnnn <thgus05061@naver.com>
2025-04-03 22:22:08 +0900

    feat: 폴더 추가 기능 구현

--- src/pages/archive/components/newFolder/NewFolderMain.tsx
+++ src/pages/archive/components/newFolder/NewFolderMain.tsx
@@ -1,7 +1,26 @@
 import { NewfolderMainImage } from "@images/index";
+import { useState } from "react";
+import { AddFolderModal } from "../modal/AddFolderModal/AddFolderModal";
 
 const NewFolderMain = () => {
-  return <img src={NewfolderMainImage} alt="new folder" className="w-[11.75rem] h-[11.75rem] cursor-pointer" />;
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(true);
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  return (
+    <>
+      <img
+        onClick={handleClick}
+        src={NewfolderMainImage}
+        alt="new folder"
+        className="w-[11.75rem] h-[11.75rem] cursor-pointer"
+      />
+      {isOpen && <AddFolderModal onClose={handleClose} />}
+    </>
+  );
 };
 
 export default NewFolderMain;
