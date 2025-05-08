6ca4965b94932b7c1657373ad7c4fd6698ceefd1
Sohyunnnn <thgus05061@naver.com>
2025-05-08 21:09:12 +0900

    feat: 노트에디터 헤더 퍼블리싱

--- src/pages/note-editor/components/button/button.tsx
+++ src/pages/note-editor/components/button/button.tsx
@@ -1,3 +1,4 @@
+import { Text } from "@components/typography/Text";
 import { cn } from "@utils/cn";
 
 interface ButtonProps {
@@ -6,5 +7,11 @@ interface ButtonProps {
 }
 
 export default function Button({ children, className }: ButtonProps) {
-  return <button className={cn(className)}>{children}</button>;
+  return (
+    <button className={cn("border border-gray-150 py-2 rounded-[4px]", className)}>
+      <Text variant="sub_heading4" className="text-gray-700">
+        {children}
+      </Text>
+    </button>
+  );
 }
--- src/pages/note-editor/components/header/header.tsx
+++ src/pages/note-editor/components/header/header.tsx
@@ -1,9 +1,24 @@
+import { EmptyStarIcon, StarIcon } from "@svgs/index";
+import { useState } from "react";
 import Button from "../button/button";
 
 const Header = () => {
+  const [isMark, setIsMark] = useState(false);
+
+  const handleMark = () => {
+    setIsMark(!isMark);
+  };
   return (
-    <header className="py-5 px-8 bg-white border">
-      <Button>내 아카이브로 돌아가기</Button>
+    <header className="py-5 px-8 bg-white border min-w-[68rem] flex justify-between">
+      <Button className="px-5">내 아카이브로 돌아가기</Button>
+      <div className="flex gap-4">
+        <button onClick={handleMark}>
+          {isMark ? <StarIcon /> : <EmptyStarIcon className="text-gray-350 m-[0.38rem]" />}
+        </button>
+
+        <Button className="px-3">저장</Button>
+        <Button className="px-3">링크 공유</Button>
+      </div>
     </header>
   );
 };
