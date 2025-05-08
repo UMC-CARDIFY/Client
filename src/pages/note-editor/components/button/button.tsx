2117f0bf987c05c322950c2648a7487d581b3b9c
Sohyunnnn <thgus05061@naver.com>
2025-05-08 22:39:19 +0900

    feat: 클릭 이벤트 추가

--- src/pages/note-editor/components/button/button.tsx
+++ src/pages/note-editor/components/button/button.tsx
@@ -1,15 +1,16 @@
 import { Text } from "@components/typography/Text";
 import { cn } from "@utils/cn";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 
-interface ButtonProps {
-  children: string;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
   className?: string;
 }
 
-export default function Button({ children, className }: ButtonProps) {
+export default function Button({ children, className, ...props }: ButtonProps) {
   return (
-    <button className={cn("border border-gray-150 py-2 rounded-[4px]", className)}>
-      <Text variant="sub_heading4" className="text-gray-700">
+    <button className={cn("border flex border-gray-150 py-2 rounded-[4px] hover:", className)} {...props}>
+      <Text variant="sub_heading4" className="text-gray-700 flex gap-[0.625rem]">
         {children}
       </Text>
     </button>
