7da02bc0596d10bbb0778f58765ce9c7ba372e68
hyesngy <tell280210@gmail.com>
2025-05-08 15:31:58 +0900

    refactor: px 단위 rem으로 변경

--- src/pages/note-editor/components/header/index.tsx
+++ src/pages/note-editor/components/header/index.tsx
@@ -1,5 +1,5 @@
 const Header = () => {
-  return <header className="h-[72px] bg-white border">Header</header>;
+  return <header className="h-[4.5rem] bg-white border">Header</header>;
 };
 
 export default Header;
--- src/pages/note-editor/components/sidebar/index.tsx
+++ src/pages/note-editor/components/sidebar/index.tsx
@@ -1,5 +1,5 @@
 const Sidebar = () => {
-  return <div className="w-[352px] min-h-full bg-white border">Sidebar</div>;
+  return <div className="w-[22rem] min-h-full bg-white border">Sidebar</div>;
 };
 
 export default Sidebar;
