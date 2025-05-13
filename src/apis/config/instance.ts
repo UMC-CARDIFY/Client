c909ae09b94268daec50cfc93359a14c79380a3e
daahyunk <ekgusdl0408@naver.com>
2025-05-13 18:42:06 +0900

    refactor: api version을 base url에 명시

--- src/apis/config/instance.ts
+++ src/apis/config/instance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER_URL}${import.meta.env.VITE_API_PREFIX}`,
+  baseURL: `${import.meta.env.VITE_SERVER_URL}${import.meta.env.VITE_API_VERSION}`,
   headers: {
     "Content-Type": "application/json",
   },
