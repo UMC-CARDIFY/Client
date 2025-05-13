d40b7ceef75455b263a2270d7f123206570aac3c
daahyunk <ekgusdl0408@naver.com>
2025-05-13 18:30:44 +0900

    refactor: ui와 도메인 로직 분리하게 수정

--- src/pages/archive/components/modal/AddFolderModal/AddFolderModal.tsx
+++ src/pages/archive/components/modal/AddFolderModal/AddFolderModal.tsx
@@ -1,28 +1,26 @@
 import { Text } from "@components/typography/Text";
-import { useColorUtils } from "@pages/archive/hooks/useColorUtils";
+import { useColorUtils } from "@pages/archive/hooks/use-color-utils";
 import { colorMap } from "@styles/colorMap";
 import { ColorCircleCheckIcon, ColorCircleIcon } from "@svgs/index";
 import React, { useState, useRef } from "react";
 import { ArchiveFolderIcon } from "../../ArchiveFolderIcon";
-import { usePostFolders } from "@pages/archive/hooks/use-folder";
 
 interface AddFolderModalProps {
+  isOpen: boolean;
   onClose: () => void;
+  onSubmit: (folderName: string, folderColor: string) => void;
 }
 
-export const AddFolderModal: React.FC<AddFolderModalProps> = ({ onClose }) => {
+export const AddFolderModal: React.FC<AddFolderModalProps> = ({ isOpen, onClose, onSubmit }) => {
   const [folderName, setFolderName] = useState("");
   const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const [selectedColor, setSelectedColor] =
-    useState<keyof typeof colorMap>("blue"); // 기본 색상
+  const [selectedColor, setSelectedColor] = useState<keyof typeof colorMap>("blue");
 
   const { darkenColor } = useColorUtils();
 
-  const { mutate } = usePostFolders();
-
   const handleSubmit = () => {
     if (folderName.trim()) {
-      mutate({ name: folderName, color: selectedColor });
+      onSubmit(folderName, selectedColor);
       setFolderName("");
       setSelectedColor("blue");
       onClose();
@@ -39,6 +37,8 @@ export const AddFolderModal: React.FC<AddFolderModalProps> = ({ onClose }) => {
       textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
     }
   };
+
+  if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center z-50">
@@ -74,11 +74,7 @@ export const AddFolderModal: React.FC<AddFolderModalProps> = ({ onClose }) => {
           </Text>
         </div>
 
-        {/* Color Picker + Icon */}
-        <Text
-          variant="sub_heading3"
-          className="block text-gray-700 mb-[1.06rem]"
-        >
+        <Text variant="sub_heading3" className="block text-gray-700 mb-[1.06rem]">
           색상
         </Text>
         <div className="flex items-center mb-12 w-[21rem] h-[3.25rem]">
@@ -95,10 +91,7 @@ export const AddFolderModal: React.FC<AddFolderModalProps> = ({ onClose }) => {
                     style={{ fill: colorMap[color as keyof typeof colorMap] }}
                   />
                 ) : (
-                  <ColorCircleIcon
-                    className="w-5 h-5"
-                    style={{ fill: colorMap[color as keyof typeof colorMap] }}
-                  />
+                  <ColorCircleIcon className="w-5 h-5" style={{ fill: colorMap[color as keyof typeof colorMap] }} />
                 )}
               </button>
             ))}
@@ -114,11 +107,7 @@ export const AddFolderModal: React.FC<AddFolderModalProps> = ({ onClose }) => {
         </div>
 
         <div className="flex justify-end gap-3">
-          <button
-            type="button"
-            onClick={onClose}
-            className="h-8 px-5 py-1 rounded-md bg-gray-50 hover:bg-gray-100"
-          >
+          <button type="button" onClick={onClose} className="h-8 px-5 py-1 rounded-md bg-gray-50 hover:bg-gray-100">
             <Text variant="sub_heading2" className="text-gray-700">
               취소
             </Text>
