d4bc48dc29504e621d7b51e5fcd86f9ebca5e2f6
hyesngy <tell280210@gmail.com>
2025-07-01 12:45:24 +0900

    feat: add tiptap editor

--- src/pages/note-editor/components/editor/index.tsx
+++ src/pages/note-editor/components/editor/index.tsx
@@ -1,5 +1,12 @@
+import { SimpleEditor } from "../tiptap-templates/simple/simple-editor";
+import "../tiptap-templates/simple/simple-editor.scss";
+
 const Editor = () => {
-  return <div className="flex h-full w-full border">Editor</div>;
+  return (
+    <div className="w-full h-full">
+      <SimpleEditor />
+    </div>
+  );
 };
 
 export default Editor;
