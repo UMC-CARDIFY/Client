30f6c174c608df1cb652fe66a014c3f8983c6940
Sohyunnnn <thgus05061@naver.com>
2025-07-10 18:19:40 +0900

    feat: 공통 버튼 구현

---
+++ src/components/common/Button/Button.stories.tsx
@@ -0,0 +1,65 @@
+import { ArrowLeftIcon } from "@pages/note-editor/components/tiptap-icons/arrow-left-icon";
+import { Meta, StoryFn } from "@storybook/react";
+import Button from "./Button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+  argTypes: {
+    variant: {
+      control: "select",
+      options: ["SIZE", "NORMAL", "STORKE", "FILL", "SMALL"],
+    },
+    size: {
+      control: "select",
+      options: ["XS", "S", "M", "L", "XL", "UNKNOWN"],
+    },
+  },
+} as Meta<typeof Button>;
+
+const Template: StoryFn<typeof Button> = (args) => <Button {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {
+  variant: "NORMAL",
+  children: "Normal Button",
+};
+
+export const Storke = Template.bind({});
+Storke.args = {
+  variant: "STORKE",
+  children: "Storke Button",
+};
+
+export const Fill = Template.bind({});
+Fill.args = {
+  variant: "FILL",
+  children: "Fill Button",
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  variant: "SMALL",
+  children: "Small Button",
+};
+
+export const SizeXS = Template.bind({});
+SizeXS.args = {
+  variant: "SIZE",
+  size: "XS",
+  children: "xs",
+};
+
+export const SizeS = Template.bind({});
+SizeS.args = {
+  variant: "SIZE",
+  size: "S",
+  children: "s",
+};
+
+export const SizeMedium = Template.bind({});
+SizeMedium.args = {
+  variant: "SIZE",
+  size: "M",
+  children: "m",
+};
