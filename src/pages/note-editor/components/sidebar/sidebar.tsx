921b3929a0941601ae60fdd1c35548073032e582
Sohyunnnn <thgus05061@naver.com>
2025-07-29 09:35:21 +0900

    feat: 버튼 추가 및 스타일링

--- src/pages/note-editor/components/sidebar/sidebar.tsx
+++ src/pages/note-editor/components/sidebar/sidebar.tsx
@@ -1,45 +1,62 @@
 import Button from "@components/common/button/button";
 import { Text } from "@components/typography/Text";
-import { HalfDoubleArrowBoldIcon, LogoIcon, SortIcon } from "@svgs/index";
+import { FolderIcon, HalfDoubleArrowBoldIcon, LogoIcon, PlusIcon, SortIcon } from "@svgs/index";
+import { useState } from "react";
 import FolderList from "../folder-list/folder-list";
 import NoteList from "../note-list/note-list";
 import Section from "../section/section";
+import Tab from "../tab/tab";
 
 const Sidebar = () => {
+  const [activeTab, setActiveTab] = useState<"폴더" | "플래시 카드">("폴더");
+
   return (
-    <div className="w-[22rem] min-h-full bg-white border">
+    <div className="w-[22rem] min-h-full bg-white border relative h-screen">
       <div className="py-4 pl-10 pr-4 flex justify-between items-center">
         <LogoIcon />
         <Button variant="SIZE" size="XL">
           <HalfDoubleArrowBoldIcon className="text-gray-400" />
         </Button>
       </div>
-      <div className="w-full h-[1px] bg-gray-150" />
       <div className="flex justify-between py-4 pl-10 pr-4">
         <div className="flex gap-2">
-          <div>폴더</div>
-          <div>플래시 카드</div>
+          <Tab isActive={activeTab === "폴더"} onClick={() => setActiveTab("폴더")}>
+            폴더
+          </Tab>
+          <Tab isActive={activeTab === "플래시 카드"} onClick={() => setActiveTab("플래시 카드")}>
+            플래시 카드
+          </Tab>
         </div>
+
         <Button variant="SIZE" size="M">
           <SortIcon className="w-6 h-6" />
         </Button>
       </div>
       <div className="w-full h-[1px] bg-gray-150" />
-      <div className="pt-4 pl-10 pr-7">
-        <div className="flex gap-2 items-center">
-          <div>아이콘</div>
-          <Text variant="sub_heading3">생물학</Text>
+      {activeTab === "폴더" && (
+        <div className="pt-4 pl-10 pr-7">
+          <>
+            <div className="flex gap-2 items-center">
+              <FolderIcon />
+              <Text variant="sub_heading3">생물학</Text>
+            </div>
+            <Section title="즐겨찾기 한 항목">
+              <FolderList>진화와 분류학</FolderList>
+            </Section>
+            <Section title="폴더">
+              <FolderList notes={["CH3. 세포막과 물질 이동", "CH3. 세포막과 물질 이동"]}>진화와 분류학</FolderList>
+            </Section>
+            <Section title="노트">
+              <NoteList>CH3. 세포막과 물질 이동</NoteList>
+            </Section>
+            <Button variant="SMALL" className="absolute bottom-[3.56rem] left-1/2 -translate-x-1/2">
+              <PlusIcon /> 추가
+            </Button>
+          </>
         </div>
-        <Section title="즐겨찾기 한 항목">
-          <FolderList>진화와 분류학</FolderList>
-        </Section>
-        <Section title="폴더">
-          <FolderList notes={["CH3. 세포막과 물질 이동", "CH3. 세포막과 물질 이동"]}>진화와 분류학</FolderList>
-        </Section>
-        <Section title="노트">
-          <NoteList>CH3. 세포막과 물질 이동</NoteList>
-        </Section>
-      </div>
+      )}
+
+      {activeTab === "플래시 카드" && <></>}
     </div>
   );
 };
