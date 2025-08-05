ec4f10f74c7d70a993d517e4efb765dba1164571
hyesngy <tell280210@gmail.com>
2025-08-05 15:04:26 +0900

    feat: Button 컴포넌트 variant 추가

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -3,7 +3,8 @@ import { cn } from "@utils/cn";
 
 interface ButtonProps {
   size?: "XS" | "S" | "M" | "L" | "XL" | "UNKNOWN";
-  variant: "SIZE" | "NORMAL" | "STROKE" | "FILL" | "SMALL";
+  variant: "SIZE" | "NORMAL" | "STROKE" | "FILL" | "SMALL" | "LONG";
+  color?: "skyblue" | "blue";
   children: React.ReactNode;
   className?: string;
 }
@@ -23,27 +24,26 @@ const textVariantMap: Record<ButtonProps["variant"], TextProps["variant"]> = {
   STROKE: "sub_heading4",
   FILL: "sub_heading4",
   SMALL: "sub_heading4",
+  LONG: "sub_heading4",
 };
 
-const Button: React.FC<ButtonProps> = ({
-  variant,
-  size = "UNKNOWN",
-  children,
-  className,
-}) => {
+const ColorMap: Record<NonNullable<ButtonProps["color"]>, string> = {
+  skyblue: "bg-brand-100 hover:bg-brand-200 active:bg-brand-300 text-brand-700",
+  blue: "bg-brand-original text-base-white",
+};
+
+const Button: React.FC<ButtonProps> = ({ variant, size = "UNKNOWN", color, children, className }) => {
   return (
     <button
       className={cn(
         `rounded flex items-center justify-center ${className}`,
         variant === "SIZE" && sizeClassMap[size],
-        variant === "NORMAL" &&
-          "px-5 py-2 bg-gray-50 hover:bg-gray-100 active:bg-gray-150",
+        variant === "NORMAL" && "px-5 py-2 bg-gray-50 hover:bg-gray-100 active:bg-gray-150",
         variant === "STROKE" &&
           "border border-gray-150 gap-[0.62rem]  px-4 py-2 bg-gray-white hover:bg-gray-50 active:bg-gray-100",
-        variant === "FILL" &&
-          "gap-[0.5rem] px-4 py-2  hover:bg-gray-100 active:bg-gray-200",
-        variant === "SMALL" &&
-          "gap-[0.5rem] px-2 py-1 bg-gray-white hover:bg-gray-50 active:bg-gray-100"
+        variant === "FILL" && "gap-2 px-4 py-2  hover:bg-gray-100 active:bg-gray-200",
+        variant === "SMALL" && "gap-2 px-2 py-1 bg-gray-white hover:bg-gray-50 active:bg-gray-100",
+        variant === "LONG" && `w-full px-2 py-3 ${color ? ColorMap[color] : ""}`,
       )}
     >
       <Text
@@ -52,8 +52,9 @@ const Button: React.FC<ButtonProps> = ({
           "flex",
           variant === "NORMAL" && "text-gray-700",
           variant === "STROKE" && "gap-[0.62rem] text-gray-500",
-          variant === "FILL" && "gap-[0.5rem] text-gray-700",
-          variant === "SMALL" && "gap-[0.5rem] text-gray-700"
+          variant === "FILL" && "gap-2 text-gray-700",
+          variant === "SMALL" && "gap-2 text-gray-700",
+          variant == "LONG" && "gap-2",
         )}
       >
         {children}
