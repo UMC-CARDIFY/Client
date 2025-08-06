d229fad443f4b42a7b0421502af24f98f2bcd9b2
daahyunk <ekgusdl0408@naver.com>
2025-08-06 19:04:33 +0900

    feat: 하위 폴더 추가 api 연동

--- src/pages/archive/components/newFolder/NewFolder.tsx
+++ src/pages/archive/components/newFolder/NewFolder.tsx
@@ -1,8 +1,17 @@
 import { NewfolderImage } from "@images/index";
 
-const NewFolder = () => {
+interface NewFolderProps {
+  onClick: () => void;
+}
+
+const NewFolder: React.FC<NewFolderProps> = ({ onClick }) => {
   return (
-    <img src={NewfolderImage} alt="new folder" className="w-[11.75rem] h-[3rem] hover:bg-gray-50 cursor-pointer" />
+    <img
+      src={NewfolderImage}
+      alt="new folder"
+      className="w-[11.75rem] h-[3rem] hover:bg-gray-50 cursor-pointer"
+      onClick={onClick}
+    />
   );
 };
 
