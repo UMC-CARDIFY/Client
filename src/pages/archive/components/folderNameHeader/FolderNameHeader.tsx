c2b2b0f16ed12c7489cd92b0ee0bb1470c2eec07
daahyunk <ekgusdl0408@naver.com>
2025-08-06 19:05:09 +0900

    feat: 폴더 이름 헤더 api 연동

--- src/pages/archive/components/folderNameHeader/FolderNameHeader.tsx
+++ src/pages/archive/components/folderNameHeader/FolderNameHeader.tsx
@@ -2,11 +2,11 @@ import Kebab from "@components/common/dropdown/Kebab";
 import { Text } from "@components/typography/Text";
 import { useColorUtils } from "@pages/archive/hooks/use-color-utils";
 import { colorMap } from "@styles/colorMap";
-import { EmptyStarIcon, StarIcon } from "@svgs/index";
-import React from "react";
-import { useState } from "react";
+import { EmptyStarIcon } from "@svgs/index";
+import React, { useState } from "react";
 import { useFolderMark } from "../../hooks/use-archive-folder";
 import { ArchiveFolderIcon } from "../ArchiveFolderIcon";
+import { StarIcon } from "../StarIcon";
 import { DeleteFolderModal } from "../modal/DeleteFolderModal/DeleteFolderModal";
 import { EditFolderModal } from "../modal/EditFolderModal/EditFolderModal";
 
@@ -14,7 +14,7 @@ interface FolderNameHeaderProps {
   folderId: number;
   folderName: string;
   color: string;
-  markState: boolean;
+  markState: "ACTIVE" | "INACTIVE";
 }
 
 const FolderNameHeader: React.FC<FolderNameHeaderProps> = ({ folderId, folderName, color, markState }) => {
@@ -26,12 +26,8 @@ const FolderNameHeader: React.FC<FolderNameHeaderProps> = ({ folderId, folderNam
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const markFolderMutation = useFolderMark();
 
-  const handleEditFolder = () => {
-    setIsEditModalOpen(false);
-  };
-  const handleDeleteFolder = () => {
-    setIsDeleteModalOpen(false);
-  };
+  const handleEditFolder = () => setIsEditModalOpen(false);
+  const handleDeleteFolder = () => setIsDeleteModalOpen(false);
 
   const handleToggleMark = (e: React.MouseEvent) => {
     e.stopPropagation();
@@ -43,21 +39,20 @@ const FolderNameHeader: React.FC<FolderNameHeaderProps> = ({ folderId, folderNam
   return (
     <div className="flex flex-row items-center h-[4rem] relative w-full">
       <button className="cursor-pointer mr-2" onClick={handleToggleMark}>
-        {markState ? <StarIcon /> : <EmptyStarIcon />}
+        {markState === "ACTIVE" ? <StarIcon /> : <EmptyStarIcon className="text-gray-400" />}
       </button>
+
       <ArchiveFolderIcon fillColor={colorHexCode} backgroundColor={darkenedColor} />
+
       <Text variant="heading2" className="text-base-black ml-4">
         {folderName}
       </Text>
+
       <div className="flex items-center absolute right-0">
         <Kebab
           onSelect={(value) => {
-            if (value === "edit") {
-              setIsEditModalOpen(true);
-            }
-            if (value === "delete") {
-              setIsDeleteModalOpen(true);
-            }
+            if (value === "edit") setIsEditModalOpen(true);
+            if (value === "delete") setIsDeleteModalOpen(true);
           }}
         />
       </div>
@@ -72,6 +67,7 @@ const FolderNameHeader: React.FC<FolderNameHeaderProps> = ({ folderId, folderNam
           folderId={folderId}
         />
       )}
+
       {isDeleteModalOpen && (
         <DeleteFolderModal
           isOpen={isDeleteModalOpen}
