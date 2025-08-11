2a475de9dbf6f49abaeb55990ae7c00fac2856d1
daahyunk <ekgusdl0408@naver.com>
2025-08-11 19:40:12 +0900

    feat: 노트 체크 토글 구현

--- src/pages/archive/components/NoteItem/NoteItem.tsx
+++ src/pages/archive/components/NoteItem/NoteItem.tsx
@@ -1,8 +1,13 @@
 import { Text } from "@components/typography/Text";
-import { ArchiveNoteIcon, CheckboxIcon, EmptyStarIcon, FlashcardIcon } from "@svgs/index";
-import { NoteItemProps } from "@typedefs";
+import { ArchiveNoteIcon, CheckboxFilledIcon, CheckboxIcon, EmptyStarIcon, FlashcardIcon } from "@svgs/index";
+import type { NoteItem as NoteItemType } from "@typedefs";
 import { useNoteMark } from "../../hooks/use-archive-note";
 import { StarIcon } from "../StarIcon";
+
+interface NoteItemProps extends NoteItemType {
+  isChecked: boolean;
+  onToggleCheck: () => void;
+}
 
 const NoteItem: React.FC<NoteItemProps> = ({
   noteId,
@@ -12,6 +17,8 @@ const NoteItem: React.FC<NoteItemProps> = ({
   flashCardCount,
   folderColor,
   markState,
+  isChecked,
+  onToggleCheck,
 }) => {
   const displayFlashcardNum = flashCardCount > 99 ? "99+" : flashCardCount;
   const { mutate } = useNoteMark();
@@ -22,24 +29,33 @@ const NoteItem: React.FC<NoteItemProps> = ({
 
   return (
     <div className="flex py-[0.5rem] items-center ml-[0.5rem]">
+      {/* 즐겨찾기 */}
       <button className="mr-[1.6rem] cursor-pointer" onClick={handleToggleMark}>
         {markState === "ACTIVE" ? <StarIcon /> : <EmptyStarIcon className="text-gray-400" />}
       </button>
 
-      <CheckboxIcon className="mr-[1.38rem] cursor-pointer" />
+      {/* 체크박스 */}
+      <button onClick={onToggleCheck} className="mr-[1.38rem] cursor-pointer">
+        {isChecked ? <CheckboxFilledIcon /> : <CheckboxIcon />}
+      </button>
+
       <ArchiveNoteIcon className={`w-[1.75rem] h-[1.75rem] mr-[1.5rem] fill-icon-${folderColor}`} />
       <div className="bg-gray-150 h-[1.5rem] w-[1px]" />
+
       <Text variant="sub_heading2" className="flex-grow pl-[1rem] pr-[1.25rem] text-base-black cursor-pointer">
         {name}
       </Text>
+
       <div className="bg-gray-150 h-[1.5rem] w-[1px]" />
       <Text variant="sub_heading3" className="text-gray-500 pl-[1rem] pr-[1.25rem]">
         {createdAt}
       </Text>
+
       <div className="bg-gray-150 h-[1.5rem] w-[1px]" />
       <Text variant="sub_heading3" className="text-gray-500 pl-[1rem] pr-[1.25rem]">
         {editDate}
       </Text>
+
       <div className="bg-gray-150 h-[1.5rem] w-[1px]" />
       <div className="w-[6.75rem]">
         <div className="inline-flex px-[0.75rem] py-[0.38rem] gap-[0.5rem] rounded-lg bg-gray-100 ml-[1rem]">
