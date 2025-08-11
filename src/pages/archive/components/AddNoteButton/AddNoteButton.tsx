a72f07e13e2c2d8e4db2c4c427923d3123684ef9
daahyunk <ekgusdl0408@naver.com>
2025-08-11 19:42:35 +0900

    style: 노트 추가 버튼 radius 수정

--- src/pages/archive/components/AddNoteButton/AddNoteButton.tsx
+++ src/pages/archive/components/AddNoteButton/AddNoteButton.tsx
@@ -2,7 +2,7 @@ import { ArchiveNoteIcon, PlusIcon } from "@svgs/index";
 
 const AddNoteButton: React.FC = () => {
   return (
-    <button className="items-center inline-flex py-[0.5rem] pr-[0.5rem] pl-[0.62rem] gap-[0.125rem] bg-gray-100 rounded-lg hover:bg-gray-200">
+    <button className="items-center inline-flex py-[0.5rem] pr-[0.5rem] pl-[0.62rem] gap-[0.125rem] bg-gray-100 rounded hover:bg-gray-200">
       <ArchiveNoteIcon className="w-4 h-4 fill-gray-450" />
       <PlusIcon className="w-4 h-4" />
     </button>
