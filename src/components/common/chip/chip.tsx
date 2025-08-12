2e1c15ca8dc6cff85769bb88298e5e8144dff574
hyesngy <tell280210@gmail.com>
2025-08-12 20:29:22 +0900

    refactor: Chip className 기본값 제거

--- src/components/common/chip/chip.tsx
+++ src/components/common/chip/chip.tsx
@@ -5,7 +5,7 @@ interface ChipProps {
   className?: string;
 }
 
-export const Chip = ({ text, className = "" }: ChipProps) => {
+export const Chip = ({ text, className }: ChipProps) => {
   return (
     <div className={`px-2 py-1 bg-brand-100 rounded inline-flex justify-center items-center gap-2 ${className}`}>
       <Text variant="caption1" className="justify-start text-brand-700 leading-none">
