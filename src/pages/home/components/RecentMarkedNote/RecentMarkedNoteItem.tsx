33f31b8ee4c072d4754f622427087aec77b44d57
hyesngy <tell280210@gmail.com>
2025-08-12 20:18:29 +0900

    refactor: Text variant에 중괄호 제거

--- src/pages/home/components/RecentMarkedNote/RecentMarkedNoteItem.tsx
+++ src/pages/home/components/RecentMarkedNote/RecentMarkedNoteItem.tsx
@@ -9,21 +9,21 @@ const RecentMarkedNoteItem: React.FC<NoteItemProps> = ({ name, content, folderCo
       <div className="pr-1 pt-5 mb-3 flex flex-row items-center">
         <StarIcon className="cursor-pointer" />
         <ArchiveNoteIcon className={`mr-2 fill-${folderColor}-600`} />
-        <Text variant={"sub_heading1"} className="w-[9.8125rem] text-base-black truncate">
+        <Text variant="sub_heading1" className="w-[9.8125rem] text-base-black truncate">
           {name}
         </Text>
       </div>
       <hr />
       <div className="flex flex-col flex-1">
-        <Text variant={"body3"} className="mt-3 px-3 h-40 text-gray-500 line-clamp-6 whitespace-normal ">
+        <Text variant="body3" className="mt-3 px-3 h-40 text-gray-500 line-clamp-6 whitespace-normal ">
           {content}
         </Text>
 
         <div className=" flex flex-row justify-between px-1 mt-auto text-gray-350">
-          <Text variant={"caption1"}>{editDate}</Text>
+          <Text variant="caption1">{editDate}</Text>
           <div className="flex flex-row gap-2">
             <FlashcardIcon />
-            <Text variant={"sub_heading3"}>{displayFlashcardNum}</Text>
+            <Text variant="sub_heading3">{displayFlashcardNum}</Text>
           </div>
         </div>
       </div>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,21 +11,21 @@ const Home = () => {
 
   return (
     <div className="w-[800px] mx-auto pb-20 text-base-black">
-      <Text variant={"heading2"} className="mt-10">
+      <Text variant="heading2" className="mt-10">
         홈
       </Text>
 
-      <Text variant={"sub_heading1"} className="mt-10 mb-6 block">
+      <Text variant="sub_heading1" className="mt-10 mb-6 block">
         예정된 학습
       </Text>
       <ScheduledLearningList items={mockScheduledLearningItems} />
 
-      <Text variant={"sub_heading1"} className="mt-14 mb-6 block">
+      <Text variant="sub_heading1" className="mt-14 mb-6 block">
         최근 즐겨찾기 한 폴더
       </Text>
       <MainFolderList variant="home" maxItems={4} />
 
-      <Text variant={"sub_heading1"} className="mt-12 mb-6 block">
+      <Text variant="sub_heading1" className="mt-12 mb-6 block">
         최근 즐겨찾기 한 노트
       </Text>
       <RecentMarkedNoteList items={mockRecentMarkedNotes} />
