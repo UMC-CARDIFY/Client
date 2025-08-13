1da460144020f1932b329fd3d359051e333b088f
daahyunk <ekgusdl0408@naver.com>
2025-08-13 21:07:05 +0900

    style: 노트 없을 때 UI 수정

--- src/pages/archive/components/EmptyState/EmptyState.tsx
+++ src/pages/archive/components/EmptyState/EmptyState.tsx
@@ -1,5 +1,5 @@
 import { Text } from "@components/typography/Text";
-import { NewFolderIcon } from "@svgs/index";
+import { EmptyNoteIcon, NewFolderIcon } from "@svgs/index";
 import { useState } from "react";
 import { usePostFolders } from "../../hooks/use-archive-folder";
 import { AddFolderModal } from "../modal/AddFolderModal/AddFolderModal";
@@ -14,7 +14,7 @@ const EMPTY_STATE_MESSAGES = {
     description: "새로운 노트를 추가하고 학습을 시작하세요.",
   },
   folder: {
-    title: "아직 생성된 폴더가 없습니다.",
+    title: "아직 생성한 폴더가 없습니다.",
     description: "새로운 폴더를 추가하고 학습을 시작하세요.",
   },
 } as const;
@@ -40,7 +40,8 @@ const EmptyState: React.FC<EmptyStateProps> = ({ type }) => {
 
   return (
     <div className="flex flex-col items-center text-gray-500">
-      <NewFolderIcon className="w-12 h-12 mb-2" />
+      {type === "folder" ? <NewFolderIcon className="w-12 h-12 mb-2" /> : <EmptyNoteIcon className="w-8 h-8 mb-2" />}
+
       <div className="flex flex-col items-center gap-1">
         <Text variant="sub_heading2">{messages.title}</Text>
         <Text variant="body3">{messages.description}</Text>
