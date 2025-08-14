c5dff970c415408a945c5577a1a0b254487f6624
Sohyunnnn <thgus05061@naver.com>
2025-08-14 18:45:48 +0900

    refactor: 플래시카드 폴더 구조 수정

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
 export { default as Home } from "./home";
 export { default as Archive } from "./archive/pages";
-export { default as Flashcard } from "./flashcard";
+export { default as Flashcard } from "./flashcard/pages/flashcard";
 export { default as Library } from "./library/pages/library";
 export { default as MyPage } from "./my-page/pages/my-page";
 export { default as Login } from "./login/pages/login";
