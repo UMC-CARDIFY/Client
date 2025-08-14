1baff03695309651340a4eb0d2ec44b5a93935ab
Sohyunnnn <thgus05061@naver.com>
2025-08-14 18:36:25 +0900

    refactor: 마이페이지 폴더 구조 수정

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,6 +2,6 @@ export { default as Home } from "./home";
 export { default as Archive } from "./archive/pages";
 export { default as Flashcard } from "./flashcard";
 export { default as Library } from "./library";
-export { default as Mypage } from "./mypage";
+export { default as MyPage } from "./my-page/pages/my-page";
 export { default as Login } from "./login/pages/login";
 export { default as Subscribe } from "./subscribe";
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import NotesInsideFolderPage from "@pages/archive/pages/NotesInsideFolderPage";
 import { createBrowserRouter } from "react-router-dom";
 import NoteLayout from "src/layouts/noteLayout";
 import MainLayout from "../layouts/mainLayout";
-import { Archive, Flashcard, Home, Library, Login, Mypage, Subscribe } from "../pages";
+import { Archive, Flashcard, Home, Library, Login, MyPage, Subscribe } from "../pages";
 import { PATHS } from "./paths";
 
 const router = createBrowserRouter([
@@ -32,7 +32,7 @@ const router = createBrowserRouter([
       },
       {
         path: PATHS.MYPAGE,
-        element: <Mypage />,
+        element: <MyPage />,
       },
       {
         path: PATHS.SUBSCRIBE,
