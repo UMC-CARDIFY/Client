d95be9c546dd4da16cffc9ab7650ebc7e49e1c5f
hyeseong <tell280210@gmail.com>
2025-12-06 16:55:17 +0900

    fix: 홈 최근 즐겨찾기한 노트 클릭 핸들러 추가

--- src/pages/home/components/RecentMarkedNote/RecentMarkedNoteItem.tsx
+++ src/pages/home/components/RecentMarkedNote/RecentMarkedNoteItem.tsx
@@ -1,11 +1,30 @@
 import { Text } from "@components/typography/Text";
+import { PATHS } from "@routes/paths";
 import { ArchiveNoteIcon, FlashcardIcon, StarIcon } from "@svgs/index";
 import { NoteItemProps } from "@typedefs";
+import { useNavigate } from "react-router";
 
-const RecentMarkedNoteItem: React.FC<NoteItemProps> = ({ name, content, folderColor, editDate, flashCardCount }) => {
+const RecentMarkedNoteItem: React.FC<NoteItemProps> = ({
+  noteId,
+  folderId,
+  name,
+  content,
+  folderColor,
+  editDate,
+  flashCardCount,
+}) => {
+  const navigate = useNavigate();
   const displayFlashcardNum = flashCardCount > 99 ? "99+" : flashCardCount;
+
+  const handleClick = () => {
+    navigate(PATHS.NOTE_EDITOR.replace(":folderId", String(folderId)).replace(":noteId", String(noteId)));
+  };
+
   return (
-    <div className="w-[15.8125rem] h-[17.5rem] shrink-0 px-5 flex flex-col pb-4 border border-solid border-gray-150 rounded-lg cursor-pointer hover:bg-brand-20">
+    <div
+      className="w-[15.8125rem] h-[17.5rem] shrink-0 px-5 flex flex-col pb-4 border border-solid border-gray-150 rounded-lg cursor-pointer hover:bg-brand-20"
+      onClick={handleClick}
+    >
       <div className="pr-1 pt-5 mb-3 flex flex-row items-center">
         <StarIcon className="cursor-pointer" />
         <ArchiveNoteIcon className={`mr-2 fill-${folderColor}-600`} />
