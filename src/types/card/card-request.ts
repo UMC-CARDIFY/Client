3fe3bf4a6a615a2f4dfcd61317b6cb1fa2a9ae3c
hyeseong <tell280210@gmail.com>
2025-12-06 15:15:08 +0900

    refactor: SortOrder 타입 정의

--- src/types/card/card-request.ts
+++ src/types/card/card-request.ts
@@ -1,5 +1,7 @@
+import type { SortOrder } from "../common";
+
 export interface FetchStudyCardsParams {
-  order?: string;
+  order?: SortOrder;
   color?: string;
   studyStatus?: number;
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+export * from "./common";
 export * from "./folder";
 export * from "./note";
 export * from "./home";
